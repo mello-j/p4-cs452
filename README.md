368fab1076affbd6abafcd5d0d711ef97f2611d1
Justin M <88345763+mello-j@users.noreply.github.com>
2025-04-30 02:30:41 +0000

    stub functions

--- README.md
+++ README.md
@@ -27,3 +27,8 @@ In order to use git send-mail you need to run the following command:
 ```bash
 make install-deps
 ```
+
+
+## Project Description
+This is the final project for CS452 that implements a FIFO queue as a monitor for the bounded buffer problem. This project is used to develop and showcase programming techniques in threading, event loops, and inter-process communciation.
+
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,45 @@
-/**Update this file with the starter code**/
+#include "lab.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition of the opaque queue structure
+struct queue {
+    int capacity;
+    int size;
+    int front;
+    int rear;
+    bool shutdown;
+    void **data;
+};
+
+queue_t queue_init(int capacity) {
+    // TODO: Implement queue initialization
+    return NULL;
+}
+
+void queue_destroy(queue_t q) {
+    // TODO: Implement queue destruction
+}
+
+void enqueue(queue_t q, void *data) {
+    // TODO: Implement adding an element to the queue
+}
+
+void *dequeue(queue_t q) {
+    // TODO: Implement removing an element from the queue
+    return NULL;
+}
+
+void queue_shutdown(queue_t q) {
+    // TODO: Implement setting the shutdown flag
+}
+
+bool is_empty(queue_t q) {
+    // TODO: Implement checking if the queue is empty
+    return false;
+}
+
+bool is_shutdown(queue_t q) {
+    // TODO: Implement checking if the queue is shutdown
+    return false;
+}
